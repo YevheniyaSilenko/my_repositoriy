5044d7feadafdc9aeba1714ff2b340a010c21b49
Yevheniia Silenko <prencezza12@gmail.com>
2024-05-20 11:04:16 +0200

    Added JavaScript code for homework tasks

---
+++ filename.js
@@ -0,0 +1,94 @@
+// Задача 1: 'number' + 3 + 3
+// Результат: "number33"
+const result1 = 'number' + 3 + 3;
+console.log(result1);
+
+// Задача 2: null + 3
+// Результат: 3
+const result2 = null + 3;
+console.log(result2);
+
+// Задача 3: 5 && "qwerty"
+// Результат: "qwerty"
+const result3 = 5 && "qwerty";
+console.log(result3);
+
+// Задача 4: +'40' + +'2' + "hillel"
+// Результат: "42hillel"
+const result4 = +'40' + +'2' + "hillel";
+console.log(result4);
+
+// Задача 5: '10' - 5 === 6
+// Результат: false
+const result5 = '10' - 5 === 6;
+console.log(result5);
+
+// Задача 6: true + false
+// Результат: 1
+const result6 = true + false;
+console.log(result6);
+
+// Задача 7: '4px' - 3
+// Результат: NaN (не число)
+const result7 = '4px' - 3;
+console.log(result7);
+
+// Задача 8: '4' - 3
+// Результат: 1
+const result8 = '4' - 3;
+console.log(result8);
+
+// Задача 9: '6' + 3 ** 0
+// Результат: "61"
+const result9 = '6' + 3 ** 0;
+console.log(result9);
+
+// Задача 10: 12 / '6'
+// Результат: 2
+const result10 = 12 / '6';
+console.log(result10);
+
+// Задача 11: '10' + (5 === 6)
+// Результат: "10false"
+const result11 = '10' + (5 === 6);
+console.log(result11);
+
+// Задача 12: null == ''
+// Результат: false
+const result12 = null == '';
+console.log(result12);
+
+// Задача 13: 3 ** (9 / 3)
+// Результат: 27
+const result13 = 3 ** (9 / 3);
+console.log(result13);
+
+// Задача 14: !!'false' == !!'true'
+// Результат: true
+const result14 = !!'false' == !!'true';
+console.log(result14);
+
+// Задача 15: 0 || '0' && 1
+// Результат: 1
+const result15 = 0 || '0' && 1;
+console.log(result15);
+
+// Задача 16: (+null == false) < 1
+// Результат: true
+const result16 = (+null == false) < 1;
+console.log(result16);
+
+// Задача 17: false && true || true
+// Результат: true
+const result17 = false && true || true;
+console.log(result17);
+
+// Задача 18: false && (false || true)
+// Результат: false
+const result18 = false && (false || true);
+console.log(result18);
+
+// Задача 19: (+null == false) < 1 ** 5
+// Результат: true
+const result19 = (+null == false) < 1 ** 5;
+console.log(result19);
