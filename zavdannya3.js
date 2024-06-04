a3fa510857594e40b0db53e652a01629554206ce
Yevheniia Silenko <prencezza12@gmail.com>
2024-06-04 19:05:37 +0300

    Task 3 added

--- zavdannya3.js
+++ zavdannya3.js
@@ -1,7 +1,15 @@
+function multiply(a, b) {
+    let result = 0;
+    for (let i = 0; i < b; i++) {
+        result += a;
+    }
+    return result;
+}
+
 function pow(x, y) {
     let result = 1;
     for (let i = 0; i < y; i++) {
-        result *= x;
+        result = multiply(result, x);
     }
     return result;
 }
