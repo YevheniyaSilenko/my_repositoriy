b7c3debcb67f03eeaebc28e5401315b2942aaa95
Yevheniia Silenko <prencezza12@gmail.com>
2024-06-07 19:13:26 +0300

    jjjj

---
+++ checkProbabilityTheory.js
@@ -0,0 +1,16 @@
+function checkProbabilityTheory(count) {
+    let even = 0, odd = 0;
+
+    for (let i = 0; i < count; i++) {
+        let randomNumber = Math.floor(Math.random() * 901) + 100;
+        randomNumber % 2 === 0 ? even++ : odd++;
+    }
+
+    console.log(`Кількість згенерованих чисел: ${count}`);
+    console.log(`Парних чисел: ${even}`);
+    console.log(`Непарних чисел: ${odd}`);
+    console.log(`Відсоток парних до непарних: ${(even / count * 100).toFixed(2)}% / ${(odd / count * 100).toFixed(2)}%`);
+}
+
+// Приклад виклику функції
+checkProbabilityTheory(52);
